0bccb8d9a05f7b51502363eb03a42b0f7090d2f9
Olha Chernysh <oastra555@gmail.com>
2024-10-22 12:02:55 +1100

    Implement Context API

---
+++ src/context/QuizeContext.js
@@ -0,0 +1,119 @@
+import { createContext, useContext, useReducer } from "react";
+
+const QuizContext = createContext();
+
+const SECS_PER_QUESTION = 30;
+
+const initialState = {
+  questions: [],
+  //'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  secondsRemaining: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore: Math.max(
+          state.points > state.highscore ? state.points : state.highscore
+        ),
+      };
+
+    case "restart":
+      // return {
+      //   ...state,
+      //   status: "ready",
+      //   index: 0,
+      //   answer: null,
+      //   points: 0,
+      // };
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
+
+    default:
+      throw new Error("Unknown action type: " + action.type);
+  }
+}
+
+function QuizProvider({ children }) {
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  return (
+    <QuizContext.Provider
+      value={{
+        questions,
+        status,
+        index,
+        answer,
+        points,
+        highscore,
+        secondsRemaining,
+
+        dispatch,
+      }}
+    >
+      {children}
+    </QuizContext.Provider>
+  );
+}
+
+function useQuiz() {
+  const context = useContext(QuizContext);
+  if (context === undefined)
+    throw new Error("useQuiz must be used within a QuizProvider");
+  return context;
+}
+
+export { QuizProvider, useQuiz };
